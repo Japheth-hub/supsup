3b25e98d1cef0f6133b0aa9f8fdaeaa2713b7539
Japheth Ramirez <japheth.ra369@gmail.com>
2024-03-27 10:05:36 -0600

    Guardando cambios -Aprendiendo nest/typescript

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,21 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+
+  list: object = {
+    number1: 5,
+    number2: 10,
+    number3: 20,
+    number4: 30,
+    number5: 40
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
+
+  postNumber(): void {
+    console.table(this.list);
+  }
+
 }
